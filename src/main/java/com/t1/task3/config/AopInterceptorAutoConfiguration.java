595d2c9529fc0864ac13f0440d47c126753077ca
Alexey Layko <Layko1982@mail.ru>
2024-05-30 22:15:58 +0300

    настройка свойств

--- src/main/java/com/t1/task3/config/AopInterceptorAutoConfiguration.java
+++ src/main/java/com/t1/task3/config/AopInterceptorAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.t1.task3.config;
 
 import com.t1.task3.filter.LoggingFilter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 public class AopInterceptorAutoConfiguration {
 
     @Bean
+    @ConditionalOnExpression("${http.aop.interceptor.enable:false}")
     public FilterRegistrationBean<LoggingFilter> loggingFilter() {
         FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new LoggingFilter());
